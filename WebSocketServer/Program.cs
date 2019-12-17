cc84bdc31a2631377f62a55c2ca42be76fac4ac7
Thomas Cournane <thomas.cournane@amcsgroup.com>
2019-12-17 16:19:32 +0000

    initial commit. Code copied from https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_server with link in place

---
+++ WebSocketServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
